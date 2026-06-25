f6056c8d94b73058e6807bd907a09e65ef28a900
caher15 <caherdev@gmail.com>
2026-06-24 22:22:11 -0600

    manage urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from myapp.views import hello
+from myapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', hello)
+    path('', views.hello),
+    path('about/', views.about)
 ]
