61a7b8201807c2ef1780eec6ce42060168c23a55
caher15 <caherdev@gmail.com>
2026-06-28 10:28:37 -0600

    prefix in urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from myapp import views
+from django.urls import path, include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello),
-    path('about/', views.about)
+    path('', include('myapp.urls')) #'' -> prefijo 'home/'
 ]
