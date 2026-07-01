54241138321801069a10c1d251a60f22a4b867f6
caher15 <caherdev@gmail.com>
2026-07-01 17:56:41 -0600

    manage params in http

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.hello),
-    path('about/', views.about)
+    path('', views.index),
+    path('about/', views.about),
+    path('hello/<str:username>', views.hello),
+    path('send/<int:id>', views.send)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello World!</h1>")
+def index(request):
+    return HttpResponse("Index Page")
+
+def hello(request, username):
+    return HttpResponse("<h1>Hello World! %s </h1>" % username)
+
+def send(request, id):
+    print(type(id))
+    return HttpResponse("<h1>Hello World! %s </h1>" % id)
 
 def about(request):
     return HttpResponse('About')
