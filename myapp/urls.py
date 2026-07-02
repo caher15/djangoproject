c796d764ab0a504ec0499cbf937e69ac6439b772
caher15 <caherdev@gmail.com>
2026-07-01 18:19:24 -0600

    manage params with models data

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('', views.index),
     path('about/', views.about),
     path('hello/<str:username>', views.hello),
-    path('send/<int:id>', views.send)
+    path('send/<int:id>', views.send),
+     path('projects/', views.projects),
+      path('tasks/<int:id>', views.tasks)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+
+from .models import Project, Task 
 
 # Create your views here.
 def index(request):
@@ -15,5 +18,13 @@ def send(request, id):
 def about(request):
     return HttpResponse('About')
 
+def projects(request):
+    projects = list(Project.objects.values())# instancia se convierte en lista de python
+    return JsonResponse(projects, safe=False) #objeto y se pasa de safe a false
+
+def tasks(request, id):
+    #tasks = Task.objects.get(id=id)
+    tasks = get_object_or_404(Task, id=id)
+    return HttpResponse('Task: %s' % tasks.title)
 
     
