93faa3b3498fe3722e2270e9199b6af596a4fccc
caher15 <caherdev@gmail.com>
2026-07-01 19:10:37 -0600

    use render

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('hello/<str:username>', views.hello),
     path('send/<int:id>', views.send),
      path('projects/', views.projects),
-      path('tasks/<int:id>', views.tasks)
+      path('tasks/', views.tasks)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from .models import Project, Task 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index Page")
+    return render(request, 'index.html')
 
 def hello(request, username):
     return HttpResponse("<h1>Hello World! %s </h1>" % username)
@@ -16,15 +16,17 @@ def send(request, id):
     return HttpResponse("<h1>Hello World! %s </h1>" % id)
 
 def about(request):
-    return HttpResponse('About')
+    return render(request, 'about.html')
 
 def projects(request):
     projects = list(Project.objects.values())# instancia se convierte en lista de python
-    return JsonResponse(projects, safe=False) #objeto y se pasa de safe a false
+    return render(request , 'projects.html')
+    #return JsonResponse(projects, safe=False) #objeto y se pasa de safe a false
 
-def tasks(request, id):
+def tasks(request):
     #tasks = Task.objects.get(id=id)
-    tasks = get_object_or_404(Task, id=id)
-    return HttpResponse('Task: %s' % tasks.title)
+    #tasks = get_object_or_404(Task, id=id)
+    return render(request, 'tasks.html')
+    #return HttpResponse('Task: %s' % tasks.title)
 
     
